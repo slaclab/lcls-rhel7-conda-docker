e91de09a871781e57383bb34ba8c4930cccffed0
jbellister-slac <jesseb@slac.stanford.edu>
2025-06-11 23:09:38 -0700

    Update script to handle being run as part of a Kubernetes workflow

--- run-pack-and-test.sh
+++ run-pack-and-test.sh
@@ -1,29 +1,48 @@
-#!/bin/sh
-ENVIRONMENT_FILE=$1
+#!/bin/bash
 
-# exit when any command fails
+# Main script for creating, testing, and packing up a conda environment for use on the SLAC network
+
 set -e
 
 umask 0022
 
-export CONDA_PKGS_DIRS=/tmp/pkgs
-export MAMBA_ROOT_PREFIX=/tmp/mamba-root
+mkdir -p /workspace
+cd /workspace
 
-# create an dpack environment
-mamba env create -y -f "$ENVIRONMENT_FILE" --prefix /tmp/$ENVIRONMENT_NAME
-conda pack --prefix /tmp/$ENVIRONMENT_NAME --ignore-missing-files --output /tmp/$ENVIRONMENT_NAME.tar.gz
+# Retrieve the yaml file to create the conda environment from
+ENVIRONMENT_NAME="$1"
+ENVIRONMENT_FILE="${ENVIRONMENT_NAME}.yml"
+ENV_PATH="/workspace/${ENVIRONMENT_NAME}"
+GITHUB_REPO_URL="https://raw.githubusercontent.com/slaclab/lcls-python3-envs/main"
+curl -L "${GITHUB_REPO_URL}/${ENVIRONMENT_NAME}.yml" -o "$ENVIRONMENT_FILE"
+
+export CONDA_PKGS_DIRS="/pkgs"
+export MAMBA_ROOT_PREFIX="/mamba-root"
+
+# create and pack environment
+mamba env create -y -f "$ENVIRONMENT_FILE" --prefix "$ENV_PATH"
+
+# Any commands that should be taken prior to packing up the environment for use.
+# This includes disabling binaries for which we want to use the operating system's version rather than conda's version,
+# and disabling activate scripts that we don't want modifying our environment.
+curl -L "${GITHUB_REPO_URL}/scripts/pre_pack.sh" -o pre_pack.sh
+chmod +x pre_pack.sh
+CONDA_PREFIX="$ENV_PATH" bash pre_pack.sh
+
+conda pack --prefix "$ENV_PATH" --ignore-missing-files --output "${ENVIRONMENT_NAME}.tar.gz"
 
 # unpack
-mkdir /tmp/test_env
-tar -xf /tmp/$ENVIRONMENT_NAME.tar.gz -C /tmp/test_env
-source /tmp/test_env/bin/activate
-/tmp/test_env/bin/conda-unpack
+mkdir test_env
+tar -xf "${ENVIRONMENT_NAME}.tar.gz" -C "test_env"
+source test_env/bin/activate
+test_env/bin/conda-unpack
 
 # run tests
-if [[ -f "/tmp/run-test.sh" ]]; then
-    bash /tmp/run-test.sh
-fi
+curl -L "${GITHUB_REPO_URL}/scripts/run_${ENVIRONMENT_NAME}_tests.sh" -o run_tests.sh
+chmod +x run_tests.sh
+bash run_tests.sh
 
-rm -rf /tmp/$ENVIRONMENT_NAME
-rm -rf /tmp/test_env
-mv "/tmp/$ENVIRONMENT_NAME.tar.gz" /output/
+rm -rf "$ENVIRONMENT_NAME"
+rm -rf "test_env"
+chmod a+r "${ENVIRONMENT_NAME}.tar.gz"
+mv "${ENVIRONMENT_NAME}.tar.gz" /output/
